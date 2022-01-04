638c9ca8da1c3207f159acc1dbdd77fb6a09a351
Ahmed Fayeq <ahmed.fayek@vodafone.com>
2022-01-04 23:31:21 +0200

    add MathStuff class

---
+++ UnitTesting/UnitTesting/MathStuff.swift
@@ -0,0 +1,39 @@
+//
+//  MathStuff.swift
+//  UnitTesting
+//
+//  Created by Ahmed Fayeq on 04/01/2022.
+//
+
+import Foundation
+
+class MathStuff{
+    
+    func getArithmaticMean(values: Double...) -> Double{
+        var sum = 0.0
+        for val in values {
+            sum += val
+        }
+        return sum / Double(values.count)
+    }
+    
+    func addNumbers(numbers: Double...) -> Double{
+        var sum = 0.0
+        for number in numbers{
+            sum += number
+        }
+        return sum
+    }
+    
+    func subtractNumbers(number1: Double, number2: Double) ->Double {
+        number1 - number2
+    }
+    
+    func multiplyNumbers(number1: Double, number2: Double) ->Double {
+        number1 * number2
+    }
+    
+    func divideNumbers(number1: Double, number2: Double) ->Double {
+        number1 / number2
+    }
+}
